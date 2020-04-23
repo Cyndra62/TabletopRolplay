67358ce94ba83b6084192ef8ec4ba6b1e2a87e60
Cyndra62 <pablomartostriguero@gmail.com>
2020-04-23 02:09:14 +0200

    UIManager y cambio entre el registro y el login

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Registro")]
+    public GameObject UIRegistro;
+    public InputField CorreoRegistro;
+    public InputField UsuarioRegistro;
+    public InputField PassRegistro;
+    public InputField ConfPassRegistro;
+    [Header("Login")]
+    public GameObject UILogin;
+    public InputField UsuarioLogin;
+    public InputField PassLogin;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ButtonToRegistro()
+    {
+        UIRegistro.SetActive(true);
+        UILogin.SetActive(false);
+    }
+
+    public void ButtonToLogin()
+    {
+        UIRegistro.SetActive(false);
+        UILogin.SetActive(true);
+    }
+    public void ButtonConfirmLogin()
+    {
+
+    }
+    public void ButtonConfirmRegister()
+    {
+        if (PassRegistro.text != "" && ConfPassRegistro.text !="" && PassRegistro.text == ConfPassRegistro.text)
+        {
+
+        }
+    }
+}
